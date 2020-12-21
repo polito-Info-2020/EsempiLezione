39ca2fb32a256185854dba73b509b50893524aa5
Fulvio Corno <fulvio.corno@polito.it>
2020-12-21 22:25:03 +0100

    Versione semplificata di 'spellcheck', con una versione del file di 'Alice' senza il testo del progetto Gutemberg

--- P8/spellcheck/spellcheck.py
+++ P8/spellcheck/spellcheck.py
@@ -1,67 +1,67 @@
-##
-#  This program checks which words in a file are not present in a list of
-#  correctly spelled words.
+# Creare un programma che analizzi i testo presente nel libro 'story.txt' e mi stampi le parole che lessicalmente
+# sono scorrette (non compaiono nell'elenco 'parole')
+
+FILE_PAROLE = 'words'
+FILE_STORIA = 'alice30.txt'
+
+
+# dizionario = un insieme contente tutte le parole del dizionario
 #
+# storia = insieme contenente tutte le parole della storia
+#
+# se non ci sono errori => storia è un sottoinsieme del dizionario
+# se ci sono errori => errori = storia - dizionario
+
+# Dato il nome di un file, restituisce un **insieme** che contiene le parole uniche presenti nel file
+# Attenzione: rimuove tutti i segni di punteggiatura
+def parole_uniche(nome_file):
+    try:
+        file = open(nome_file, 'r')
+    except IOError:
+        print(f"Errore nell'apertura del file {nome_file}")
+        exit()
+
+    insieme = set()
+
+    for line in file:
+        parole = line.rstrip().replace('-', ' ').split()
+        for parola in parole:
+            parola_pulita = pulisci(parola)
+            if parola_pulita != '':
+                insieme.add(parola_pulita.lower())
+
+    file.close()
+    return insieme
+
+
+# rimuove tutti i caratteri INIZIALI e FINALI non alfabetici da una stringa
+def pulisci(parola):
+    pulita = parola
+
+    while len(pulita) > 0 and not pulita[0].isalpha():
+        pulita = pulita[1:]
+
+    while len(pulita) > 0 and not pulita[-1].isalpha():
+        pulita = pulita[:-1]
+
+    return pulita
+
 
 def main():
-    # Read the word list and the document.
-    correctlySpelledWords = readWords("words", "")
-    documentWords = readWords("alice30.txt", "*END*")  # Skip the prefix
+    dizionario = parole_uniche(FILE_PAROLE)
+    # print(dizionario)
 
-    # Print all words that are in the document but not the word list.
-    misspellings = documentWords.difference(correctlySpelledWords)
-    for word in sorted(misspellings):
-        print(word)
+    storia = parole_uniche(FILE_STORIA)
 
+    print(f'La storia ha un totale di {len(storia)} parole distinte tra loro')
 
-## Reads all words from a file.
-#  @param filename the name of the file
-#  @param skipUntil skip all lines until a line starts with this string
-#  @return a set with all lowercased words in the file. Here, a word is a 
-#  sequence of upper- and lowercase letters
-#   
-def readWords(filename, skipUntil):
-    wordSet = set()
-    inputFile = open(filename, "r")
-    skip = True
-
-    for line in inputFile:
-        line = line.strip()
-        if not skip:
-            # Use any character other than a-z or A-Z as word delimiters.
-            # parts = split("[^a-zA-Z]+", line)
-            parts = extractWords(line)
-            for word in parts:
-                if len(word) > 0:
-                    wordSet.add(word.lower())
-        elif line.find(skipUntil) >= 0:
-            skip = False
-
-    inputFile.close()
-    return wordSet
+    if storia.issubset(dizionario):
+        print("La storia non ha errori lessicali")
+    else:
+        parole_sbagliate = storia.difference(dizionario)
+        print("Le parole sbagliate sono:")
+        # print(parole_sbagliate)
+        print(sorted(parole_sbagliate))
 
 
-## Split a line into its words
-# Returns a list of strings, each string is an individual word
-# Words may be separated by ANY character (spaces, numbers, punctuation, ...)
-# All non-alpha characters act as delimiters. Multiple delimiters are merged (they count as one).
-def extractWords(line):
-    words = []
-    i = 0
-    while i < len(line):
-        # get a consecutive sequence of alphabetic characters
-        word = ''
-        while i < len(line) and line[i].isalpha():
-            word = word + line[i]
-            i += 1
-        if len(word) > 1:
-            words.append(word)
-
-        # now skip all non-alpha characters
-        while i < len(line) and not line[i].isalpha():
-            i += 1
-    return words
-
-
-# Start the program.
 main()
